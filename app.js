d71796c44c6ae1494cafc13e88b877fc4f4978f2
sayman <mdrabbi.asm@gmail.com>
2022-03-01 00:34:06 +0600

    add details

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const showData = (data) => {
           <div class="card-body">
             <h5 class="card-title">${x.phone_name}</h5>
             <p class="card-text">${x.brand}</p>
-            <button class="btn btn-primary"onclick="showDetails('${x.slug}')">Show Details</button>
+            <button class="btn btn-primary"onclick="showDetailsData('${x.slug}')">Show Details</button>
           </div>
         </div>
       </div>
@@ -61,9 +61,41 @@ const showData = (data) => {
         cardParent.appendChild(div);
     })
 }
-// ------Show Details----
-const showDetails = (details) => {
+// ------load Details data----
+const showDetailsData = (details) => {
     // set new url---- 
     const url = `https://openapi.programming-hero.com/api/phone/${details}`;
+    fetch(url)
+        .then(res => res.json())
+    .then(data => showDetails(data.data))
     
+}
+// ------show details----
+const showDetails = (detailsData) => {
+    console.log(detailsData);
+    const popupContainer = document.getElementById('popup-container');
+    popupContainer.innerHTML = `
+    <div class="justify-content-center align-items-center d-flex w-100 h-100" >
+             <div class="card mb-3 w-50">
+             <div class="row g-0">
+               <div class="col-md-12 col-lg-6 col-12  p-3">
+                 <img src="${detailsData.image}" class="w-100  rounded-start" alt="">
+               </div>
+               <div class="col-md-12 col-lg-6 col-12 p-3">
+                 <div class="card-body">
+                 <button class ="btn btn-danger mb-3">HOT</button>
+                   <h5 class="card-title text-danger fw-bold">Phone Name: ${detailsData.name}</h5>
+                   <h5 class="card-title">Phone Release Date: ${detailsData.releaseDate? detailsData.releaseDate:"Date Is not Available"}</h5>
+                   <p class="card-text"></p>
+                   <p class="fw-bold text-capitalize text-dark">Main Feature:</p>
+                   <p>1.Storages: ${detailsData.mainFeatures.storage}</p>
+                   <p>2.ChipSet: ${detailsData.mainFeatures.chipSet} </p>
+                   <p>2.DisplaySize: ${detailsData.mainFeatures.displaySize} </p>
+                 </div>
+               </div>
+             </div>
+           </div>
+      </div>
+    `  
+    popupContainer.style.display = 'block';
 }
