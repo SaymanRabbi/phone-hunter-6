81d894440682d4773752dcaae6503a197f259cbc
sayman <mdrabbi.asm@gmail.com>
2022-02-28 21:29:32 +0600

    add error handelar and error function

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 // ------input----
 const input = document.getElementById('input');
+// -----Error Function-------
+const errorText = document.getElementById('error-messages');
+errorText.style.display = "none";
+const error = (value) => {
+    errorText.style.display = value;
+}
 // ----search button------
 document.getElementById('search-button').addEventListener('click', () => {
     // -------input value with text And Case Sensitive-----
     const inputText = input.value.toLowerCase();
     input.value = '';
-    // ------add api url-------
+    if (inputText === '') {
+        error('block')
+    }
+    else {
+        error('none')
+        // ------add api url-------
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`;
     // add fetch..
     fetch(url)
         .then(res => res.json())
         .then(data => console.log(data));
+    }
 })
