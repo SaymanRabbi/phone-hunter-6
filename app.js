b5d051cd80a0e3d713bd742968d1038874bd198a
sayman <mdrabbi.asm@gmail.com>
2022-02-28 21:09:07 +0600

    add api url and add fetch mathod

--- app.js
+++ app.js
@@ -2,7 +2,13 @@
 const input = document.getElementById('input');
 // ----search button------
 document.getElementById('search-button').addEventListener('click', () => {
-    // -------input value with text-----
-    const inputText = input.value;
-    console.log(inputText)
+    // -------input value with text And Case Sensitive-----
+    const inputText = input.value.toLowerCase();
+    input.value = '';
+    // ------add api url-------
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`;
+    // add fetch..
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
 })
