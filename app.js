be965c506edae83308069d783310c234a506798c
sayman <mdrabbi.asm@gmail.com>
2022-02-28 20:49:54 +0600

    check input value in console

---
+++ app.js
@@ -0,0 +1,8 @@
+// ------input----
+const input = document.getElementById('input');
+// ----search button------
+document.getElementById('search-button').addEventListener('click', () => {
+    // -------input value with text-----
+    const inputText = input.value;
+    console.log(inputText)
+})
